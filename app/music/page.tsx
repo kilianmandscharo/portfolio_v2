af90a00da4eccabcc09192fe1a1dbfa365f8c015
Kilian <kilianmandscharo@gmail.com>
2023-08-11 21:48:44 +0200

    Add first piece to music section

--- app/music/page.tsx
+++ app/music/page.tsx
@@ -1,5 +1,14 @@
 import H1 from "@/components/H1";
+import H2 from "@/components/H2";
 
 export default function Music() {
-  return <H1>Music</H1>;
+  return (
+    <>
+      <H1>Music</H1>
+      <H2>Prelude to Christmas</H2>
+      <audio controls>
+        <source src="/music/christmas_prelude_c_minor.mp3" type="audio/mpeg" />
+      </audio>
+    </>
+  );
 }
