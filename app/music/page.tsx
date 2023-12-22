7f135555e98d3ee3e3c08afa7fff0ba5c1b8fe0c
Dominik Heller <kilianmandscharo@gmail.com>
2023-12-22 11:04:17 +0100

    Add new christmas prelude

--- app/music/page.tsx
+++ app/music/page.tsx
@@ -5,10 +5,20 @@ export default function Music() {
   return (
     <>
       <H1>Music</H1>
-      <H2>Prelude to Christmas</H2>
-      <audio controls>
-        <source src="/music/christmas_prelude_c_minor.mp3" type="audio/mpeg" />
-      </audio>
+      <div className="flex flex-col gap-8">
+        <div>
+          <H2>Prelude to Christmas (2022)</H2>
+          <audio controls className="rounded-lg">
+            <source src="/music/christmas_prelude_c_minor.mp3" type="audio/mpeg" />
+          </audio>
+        </div>
+        <div>
+          <H2>Prelude to Christmas (2023)</H2>
+          <audio controls className="rounded-lg">
+            <source src="/music/christmas_prelude_d_minor.mp3" type="audio/mpeg" />
+          </audio>
+        </div>
+      </div>
     </>
   );
 }
