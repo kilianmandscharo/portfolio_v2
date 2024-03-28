19904dcbf722c4d575c03d65fbb7bc5174bde97c
Dominik Heller <kilianmandscharo@gmail.com>
2024-03-28 20:29:04 +0100

    Add margin to circle h2

--- app/_components/H2WithCircle.tsx
+++ app/_components/H2WithCircle.tsx
@@ -4,7 +4,7 @@ interface Props {
 
 export default function H2WithChildren({ children }: Props) {
   return (
-    <div className="flex items-center gap-2 mb-1">
+    <div className="flex items-center gap-2 mb-1 mt-8">
       <div className="rounded-full w-3 h-3 bg-primary-300 mb-[3px]" />
       <h2 className="text-2xl font-bold font-body">{children}</h2>
     </div>
